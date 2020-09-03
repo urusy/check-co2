e3ad222d9e52184092629737fed56746dc4a372e
urushima.hiroyoshi <urushima0613@gmail.com>
2020-09-03 22:46:45 +0900

    add json modify

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from google.cloud import bigquery
 import datetime
+import json
 
 project = "urusy-1"
 dataset = "co2"
-table = "MH_Z19"
+table = "mh_z19"
+
+sensor_json = '{"co2": 611, "temperature": 25, "TT": 65, "SS": 0, "UhUl": 34816}'
+
+sensor_data = json.loads(sensor_json)
 
 bigquery_client = bigquery.Client()
-query = "INSERT INTO `{0}.{1}.{2}` VALUES (TIMESTAMP('{3}'), {4}, {5}, {6}, {7}, {8})".format(project, dataset, table, datetime.datetime.now(), 1.1, 2.2, 3, 4, 5)
+query = "INSERT INTO `{0}.{1}.{2}` VALUES (TIMESTAMP('{3}'), {4}, {5}, {6}, {7}, {8})".format(project, dataset, table, datetime.datetime.now(), sensor_data["co2"], sensor_data["temperature"], sensor_data["TT"], sensor_data["SS"], sensor_data["UhUl"])
 
 rows = bigquery_client.query(query).result()
